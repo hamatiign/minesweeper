ada7cabc6baffc77c3bcdf784fb17bdfa3ed08fd
hamatiign <hamati_ign@outlook.jp>
2025-05-22 15:52:30 +0900

    初クリック時以外もボムができてしまう

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
+//今のところ関係ない
 const calcTotal = (array: number[], counter: number) => {
   let ans: number = 0;
   for (let i = 0; i < array.length; i++) {
@@ -10,6 +11,15 @@ const calcTotal = (array: number[], counter: number) => {
   }
   return ans + counter;
 };
+
+//ボム生成用の乱数  添え字を返す
+const getRandomValue = (boardlength: number): number => {
+  return Math.floor(Math.random() * boardlength);
+};
+
+//TODO  後でstateやコンテキストでレベル別で出来るようにする
+const bombnum = 10;
+const boardlength = 8;
 
 export default function Home() {
   const [userInput, setuserInput] = useState([
@@ -38,16 +48,28 @@ export default function Home() {
 
   const clickHandler = (x: number, y: number) => {
     //初チェック時の爆弾生成
-    if (newuserInput.some((row) => row.some((num) => num === 1) === true)) {
-      newbombMap;
+    if (!newuserInput.some((row) => row.some((num) => num === 1))) {
+      for (let i = 0; i < bombnum; i++) {
+        const bom_x = getRandomValue(boardlength);
+        const bom_y = getRandomValue(boardlength);
+        if (bom_x === x && bom_y === y) {
+          i--;
+          continue;
+        }
+        newbombMap[bom_x][bom_y] = 1;
+      }
     }
+
+    console.log(newbombMap);
+    setuserInput(newuserInput);
+    setbombMap(newbombMap);
   };
   //=========================================================
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {userInput.map((row, y) =>
-          row.map((num, x) => (
+          row.map((column, x) => (
             <div key={`${x}-${y}`} onClick={() => clickHandler(x, y)} className={styles.cell} />
           )),
         )}
